aa2fb73d75a84a61fe0f64e0a60bf8b3d4c15c73
Zac Sweers <pandanomic@gmail.com>
2024-07-16 18:09:54 -0400

    Check for dagger creator annotations in KSP
    Resolves #15

---
+++ compiler/src/test/java/com/squareup/anvil/compiler/KspMergeAnnotationsCheckSymbolProcessorTest.kt
@@ -0,0 +1,214 @@
+package com.squareup.anvil.compiler
+
+import com.google.common.truth.Truth.assertThat
+import com.squareup.anvil.compiler.api.ComponentMergingBackend
+import com.squareup.anvil.compiler.internal.testing.AnvilCompilationMode
+import com.squareup.anvil.compiler.internal.testing.ComponentProcessingMode
+import com.tschuchort.compiletesting.KotlinCompilation.ExitCode
+import org.junit.Test
+
+class KspMergeAnnotationsCheckSymbolProcessorTest {
+  @Test fun `dagger creator annotations - component factory`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import dagger.Component
+      import com.squareup.anvil.annotations.MergeComponent
+      
+      @MergeComponent(Any::class)
+      interface ComponentInterface {
+        @Component.Factory
+        interface Factory {
+          fun create(): ComponentInterface
+        }
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+      componentMergingBackend = ComponentMergingBackend.KSP,
+      mode = AnvilCompilationMode.Ksp(),
+      expectExitCode = ExitCode.COMPILATION_ERROR,
+    ) {
+      assertThat(messages).contains(
+        "When using @MergeComponent, you must use @MergeComponent.Factory instead " +
+          "of Dagger's own 'dagger.Component.Factory' annotation. The Dagger " +
+          "annotation will be generated in the final merged component.",
+      )
+    }
+  }
+
+  @Test fun `dagger creator annotations - component builder`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import dagger.Component
+      import com.squareup.anvil.annotations.MergeComponent
+      
+      @MergeComponent(Any::class)
+      interface ComponentInterface {
+        @Component.Builder
+        interface Builder {
+          fun build(): ComponentInterface
+        }
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+      componentMergingBackend = ComponentMergingBackend.KSP,
+      mode = AnvilCompilationMode.Ksp(),
+      expectExitCode = ExitCode.COMPILATION_ERROR,
+    ) {
+      assertThat(messages).contains(
+        "When using @MergeComponent, you must use @MergeComponent.Builder instead " +
+          "of Dagger's own 'dagger.Component.Builder' annotation. The Dagger " +
+          "annotation will be generated in the final merged component.",
+      )
+    }
+  }
+
+  @Test fun `dagger creator annotations - subcomponent factory`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import dagger.Subcomponent
+      import com.squareup.anvil.annotations.MergeSubcomponent
+      
+      @MergeSubcomponent(Any::class)
+      interface SubcomponentInterface {
+        @Subcomponent.Factory
+        interface Factory {
+          fun create(): SubcomponentInterface
+        }
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+      componentMergingBackend = ComponentMergingBackend.KSP,
+      mode = AnvilCompilationMode.Ksp(),
+      expectExitCode = ExitCode.COMPILATION_ERROR,
+    ) {
+      assertThat(messages).contains(
+        "When using @MergeSubcomponent, you must use @MergeSubcomponent.Factory instead " +
+          "of Dagger's own 'dagger.Subcomponent.Factory' annotation. The Dagger " +
+          "annotation will be generated in the final merged component.",
+      )
+    }
+  }
+
+  @Test fun `dagger creator annotations - subcomponent builder`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import dagger.Subcomponent
+      import com.squareup.anvil.annotations.MergeSubcomponent
+      
+      @MergeSubcomponent(Any::class)
+      interface SubcomponentInterface {
+        @Subcomponent.Builder
+        interface Builder {
+          fun build(): SubcomponentInterface
+        }
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+      componentMergingBackend = ComponentMergingBackend.KSP,
+      mode = AnvilCompilationMode.Ksp(),
+      expectExitCode = ExitCode.COMPILATION_ERROR,
+    ) {
+      assertThat(messages).contains(
+        "When using @MergeSubcomponent, you must use @MergeSubcomponent.Builder instead " +
+          "of Dagger's own 'dagger.Subcomponent.Builder' annotation. The Dagger " +
+          "annotation will be generated in the final merged component.",
+      )
+    }
+  }
+
+  @Test fun `dagger creator annotations - mergecomponent factory`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import com.squareup.anvil.annotations.MergeComponent
+      
+      @MergeComponent(Any::class)
+      interface ComponentInterface {
+        @MergeComponent.Factory
+        interface Factory {
+          fun create(): ComponentInterface
+        }
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+      componentMergingBackend = ComponentMergingBackend.KSP,
+      mode = AnvilCompilationMode.Ksp(),
+      expectExitCode = ExitCode.OK,
+    )
+  }
+
+  @Test fun `dagger creator annotations - mergecomponent builder`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import com.squareup.anvil.annotations.MergeComponent
+      
+      @MergeComponent(Any::class)
+      interface ComponentInterface {
+        @MergeComponent.Builder
+        interface Builder {
+          fun build(): ComponentInterface
+        }
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+      componentMergingBackend = ComponentMergingBackend.KSP,
+      mode = AnvilCompilationMode.Ksp(),
+      expectExitCode = ExitCode.OK,
+    )
+  }
+
+  @Test fun `dagger creator annotations - mergesubcomponent factory`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import com.squareup.anvil.annotations.MergeSubcomponent
+      
+      @MergeSubcomponent(Any::class)
+      interface SubcomponentInterface {
+        @MergeSubcomponent.Factory
+        interface Factory {
+          fun create(): SubcomponentInterface
+        }
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+      componentMergingBackend = ComponentMergingBackend.KSP,
+      mode = AnvilCompilationMode.Ksp(),
+      expectExitCode = ExitCode.OK,
+    )
+  }
+
+  @Test fun `dagger creator annotations - mergesubcomponent builder`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import com.squareup.anvil.annotations.MergeSubcomponent
+      
+      @MergeSubcomponent(Any::class)
+      interface SubcomponentInterface {
+        @MergeSubcomponent.Builder
+        interface Builder {
+          fun build(): SubcomponentInterface
+        }
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+      componentMergingBackend = ComponentMergingBackend.KSP,
+      mode = AnvilCompilationMode.Ksp(),
+      expectExitCode = ExitCode.OK,
+    )
+  }
+}
