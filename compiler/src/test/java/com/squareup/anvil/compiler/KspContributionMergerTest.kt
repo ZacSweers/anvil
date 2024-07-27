36fd59cc2ac4d1dea0a063299b08183b7f4f5feb
Zac Sweers <pandanomic@gmail.com>
2024-07-27 12:24:37 -0400

    Add a failing test

--- compiler/src/test/java/com/squareup/anvil/compiler/KspContributionMergerTest.kt
+++ compiler/src/test/java/com/squareup/anvil/compiler/KspContributionMergerTest.kt
@@ -6,13 +6,18 @@ import com.squareup.anvil.compiler.internal.testing.AnvilCompilationMode
 import com.squareup.anvil.compiler.internal.testing.ComponentProcessingMode
 import com.tschuchort.compiletesting.KotlinCompilation
 import org.junit.Assume.assumeTrue
+import org.junit.Before
 import org.junit.Test
 import kotlin.reflect.KVisibility
 
 class KspContributionMergerTest {
 
-  @Test fun `creator-less components still generate a shim`() {
+  @Before
+  fun setup() {
     assumeTrue(includeKspTests())
+  }
+
+  @Test fun `creator-less components still generate a shim`() {
     compile(
       """
       package com.squareup.test
@@ -33,7 +38,6 @@ class KspContributionMergerTest {
   }
 
   @Test fun `merged component visibility is inherited from annotated class`() {
-    assumeTrue(includeKspTests())
     compile(
       """
       package com.squareup.test
@@ -55,7 +59,6 @@ class KspContributionMergerTest {
   }
 
   @Test fun `typealiases are followed`() {
-    assumeTrue(includeKspTests())
     compile(
       """
       package com.squareup.test
@@ -92,7 +95,6 @@ class KspContributionMergerTest {
   }
 
   @Test fun `error type annotations are ignored`() {
-    assumeTrue(includeKspTests())
     compile(
       """
       package com.squareup.test
@@ -119,6 +121,62 @@ class KspContributionMergerTest {
         .single { it.name == "MergedComponentInterface.kt" }
       assertThat(mergedComponent).isNotNull()
       assertThat(mergedComponent.readText()).doesNotContain("DoesNotExist")
+    }
+  }
+
+  @Test fun `factory contributors are picked up`() {
+    compile(
+      """
+      package com.squareup.test
+      
+      import com.squareup.anvil.annotations.MergeComponent
+      import com.squareup.anvil.annotations.MergeSubcomponent
+      import com.squareup.anvil.annotations.ContributesTo
+      import com.squareup.anvil.annotations.optional.SingleIn
+      import dagger.BindsInstance
+
+      abstract class AppScope private constructor()
+      abstract class UserScope private constructor()
+
+      @SingleIn(AppScope::class)
+      @MergeComponent(AppScope::class)
+      interface AppComponent {
+        @MergeComponent.Factory
+        interface Factory {
+          fun create(
+            @BindsInstance value: Int,
+          ): AppComponent
+        }
+      }
+
+      @SingleIn(UserScope::class)
+      @MergeSubcomponent(UserScope::class)
+      interface UserComponent {
+      
+        val value: Int
+      
+        @ContributesTo(AppScope::class)
+        interface Parent {
+          val userComponentFactory: Factory
+        }
+      
+        @MergeSubcomponent.Factory
+        interface Factory {
+          fun create(): UserComponent
+        }
+      }
+
+      fun caller() {
+        val parent = DaggerAppComponent.factory().create(5) as UserComponent.Parent
+        val userComponent = parent.userComponentFactory.create()
+      }
+      """,
+      componentProcessingMode = ComponentProcessingMode.KSP,
+    ) {
+      // TODO assert we can get an instance of the UserComponent
+      walkGeneratedFiles(AnvilCompilationMode.Ksp()).forEach {
+        println(it.readText())
+      }
     }
   }
 }
