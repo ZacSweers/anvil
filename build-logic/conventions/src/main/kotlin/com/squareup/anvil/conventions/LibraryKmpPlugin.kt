af04658427b10f0beab2af66919feca4220270a5
Nacho Lopez <nacho.lopez@thetradedesk.com>
2024-10-24 10:00:57 +0200

    Apply suggestions

--- build-logic/conventions/src/main/kotlin/com/squareup/anvil/conventions/LibraryKmpPlugin.kt
+++ build-logic/conventions/src/main/kotlin/com/squareup/anvil/conventions/LibraryKmpPlugin.kt
@@ -1,13 +1,9 @@
 package com.squareup.anvil.conventions
 
-import com.rickbusarow.kgx.javaExtension
 import com.rickbusarow.kgx.pluginId
 import com.squareup.anvil.conventions.utils.libs
-import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
@@ -50,6 +46,8 @@ class LibraryKmpPlugin : Plugin<Project> {
       watchosArm64()
       watchosSimulatorArm64()
       watchosX64()
+
+      mingwX64()
 
       applyDefaultHierarchyTemplate()
     }
